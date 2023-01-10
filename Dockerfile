a9d7b10cfc4176a4fd9048c1038fb348a843563e
Nicolas Ribeiro <commit@niskii.dev>
2023-01-09 23:01:12 -0300

    fix: docker container

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,15 @@
 FROM node:18.12.1
 
-COPY ./package.json .
-COPY ./yarn.lock .
-COPY tsconfig.json .
+COPY . .
 
 RUN yarn install --frozen-lockfile --non-interactive
+
+RUN yarn build
 
 COPY . .
 
 ENV PORT 80
 
-RUN yarn build
-
-RUN yarn install --production --frozen-lockfile --non-interactive --prefer-offline
-
 EXPOSE 80
 
-CMD [ "yarn", "start" ]
+CMD [ "yarn", "dev" ]
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import type { INestApplication } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let app: INestApplication
+
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const logger = new Logger('Main')
+	app = await NestFactory.create(AppModule);
 
 	app.useGlobalPipes(new ValidationPipe());
 
 	await app.listen(process.env.PORT);
+	logger.log(`Process is running on ${await app.getUrl()}`)
 }
 
 void bootstrap();
